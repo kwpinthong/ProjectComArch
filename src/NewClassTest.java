60446a9652c7228458bc3202a86b5aad0ba5328e
kandation <akkhaporn@gmail.com>
2017-10-20 15:46:28 +0700

    Test New Class

---
+++ src/NewClassTest.java
@@ -0,0 +1,10 @@
+import static java.lang.System.*;
+
+/**
+ * Created by Kanda on 20/10/.
+ */
+public class NewClassTest {
+    public static void main(String[] args) {
+        System.out.println("dddd");
+    }
+}
