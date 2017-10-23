320e34f8a425b5cc88b182cbaa3fe95587cb3ca8
ASUS_kwpinthong <kw.pinthong@gmail.com>
2017-10-23 15:33:59 +0700

    change -> keep all data in 2D array

---
+++ src/instruction/lw.java
@@ -0,0 +1,8 @@
+package instruction;
+
+/**
+ * Created by ASUS on 10/23/2017.
+ */
+public class lw {
+
+}
---
+++ src/main.java
@@ -0,0 +1,32 @@
+/**
+ * Created by ASUS on 10/18/2017.
+ */
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) throws IOException {
+        //----Read file and input each line in array-list----//
+        Scanner assemblyLine = new Scanner(new File("example_testadd.txt"));
+        List<String> line = new ArrayList<>();
+        while (assemblyLine.hasNextLine()){line.add(assemblyLine.nextLine());}
+        String[] temp = line.toArray(new String[0]); //Add every lines form array-list to array
+        //---------------------------------------------------//
+        //----------Create 2D Array to keep data-------------//
+        final int nCols = 6;
+        String[][] data = new String[temp.length][nCols];
+        for(int i = 0; i < temp.length ; i++){
+            for(int j = 0; j < nCols ; j++){
+                data[i] = temp[i].split("\t",-1);
+            }
+        }
+        //----------------------------------------------------/
+        assembly assembly = new assembly(data,temp.length,nCols);
+        assembly.working();
+        assembly.print();
+    }
+}
