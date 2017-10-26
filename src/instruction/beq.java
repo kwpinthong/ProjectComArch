bc920898c5e1d07e0bf7ff7c40550598144866c5
magmanex <timecrisis1995@live.com>
2017-10-26 23:54:07 +0700

    Update -beq(Label didn't finish yet)

--- src/instruction/beq.java
+++ src/instruction/beq.java
@@ -1,13 +1,44 @@
 package instruction;
 
+import java.util.ArrayList;
+
 public class beq {
     static String op = "100";
     register rs;
     register rt;
+    int sum;
+    String offField;
 
-    public beq (String field0,String field1)
+
+    public beq (String field0, String field1, String field2, ArrayList<String> label)
     {
         this.rs = new register(field0);
         this.rt = new register(field1);
+        offField = field2;
+
     }
+
+    public String doBeq ()
+    {
+
+        if(offField.matches("(.*)[a-z](.*)"))
+        {
+            return offField;
+        }
+        else return String.valueOf(Integer.parseInt("0000000"+op+rs.CheckReg()+rt.CheckReg()+toBinary(Integer.parseInt(offField)),2));
+    }
+    public static String toBinary(int int1){
+        int count;
+        String tmp = Integer.toBinaryString(int1);
+        count = tmp.length();
+        System.out.println(tmp);
+        while (count < 16) {
+            tmp = "0" + tmp;
+            count++;
+        }
+        return tmp;
+    }
+
+
 }
+
