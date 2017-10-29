0da6a0390cecad6999306e1e6c7324f741296b58
thanaphom <pamdhumawong@gmail.com>
2017-10-29 14:48:15 +0700

    Update -beq(correct address but didn't test anymore) -แก้ตัวแปรที่ลืมลบ

--- src/instruction/jalr.java
+++ src/instruction/jalr.java
@@ -8,9 +8,9 @@ public class jalr {
     register rs;
     register rt;
 
-    public jalr(String field0, String field1, String field2){
+    public jalr(String field0, String field1){
     this.rs = new register(field0);
-    this.rt = new register(field2);
+    this.rt = new register(field1);
     }
     public String tojalr(){
         return String.valueOf(Integer.parseInt("0000000"+op+rs.CheckReg()+rt.CheckReg()+"0000000000000000000",2));
